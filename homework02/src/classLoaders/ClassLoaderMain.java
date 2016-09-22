922f3958d22dcd25eb875d9d2ae595885c6912f8
CyberPuck <traverskyle9@gmail.com>
2016-09-22 19:58:24 -0400

    Adding part 1

---
+++ homework02/src/classLoaders/ClassLoaderMain.java
@@ -0,0 +1,10 @@
+package classLoaders;
+
+public class ClassLoaderMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ homework02/src/classLoaders/MyCustomClassLoader.java
@@ -0,0 +1,7 @@
+package classLoaders;
+
+import java.net.URLClassLoader;
+
+public class MyCustomClassLoader extends URLClassLoader {
+
+}
