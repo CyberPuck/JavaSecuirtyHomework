d0fb8e6d2344990267188cd2a846b30dec9bb3ce
Kyle <traverskyle9@gmail.com>
2016-09-24 15:45:09 -0400

    Initial code for part 1, need to fix load error

--- homework02/src/classLoaders/ClassLoaderMain.java
+++ homework02/src/classLoaders/ClassLoaderMain.java
@@ -1,10 +1,24 @@
 package classLoaders;
 
+import java.net.URL;
+
 public class ClassLoaderMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// printing PWD
+		// TODO: Remove
+//		System.out.println("Working Directory = " +
+//	              System.getProperty("user.dir"));
+		// feeding in the relative location of the test class
+		String classLocation = "../DMZ/TestClass.class";
+		URL[] urls = {};
+		MyCustomClassLoader loader = new MyCustomClassLoader(urls);
+		try {
+			Class test = loader.findClass(classLocation);
+			System.out.println(test.hashCode());
+		} catch(ClassNotFoundException e) {
+			System.err.println("Loader failed to load class");
+		}
 	}
 
 }
--- homework02/src/classLoaders/MyCustomClassLoader.java
+++ homework02/src/classLoaders/MyCustomClassLoader.java
@@ -1,7 +1,45 @@
 package classLoaders;
 
+import java.io.IOException;
+import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class MyCustomClassLoader extends URLClassLoader {
 
+	public MyCustomClassLoader(URL[] urls) {
+		super(urls);
+		System.out.println("Let URLClassLoader handle construction");
+	}
+	
+	@Override
+	protected Class<?> findClass(String name) throws ClassNotFoundException {
+		// TODO Auto-generated method stub
+		byte[] b = loadMyClassData(name);
+		return (b == null) ? null : defineClass(name, b, 0, b.length);
+	}
+
+	/**
+	 * Given the string which represents the file location, read the file as binary
+	 * data for the class loader to define the class.
+	 * @param name of class to load
+	 * @return byte array of read in class or NULL if failed
+	 */
+	private byte[] loadMyClassData(String name) {
+		if(!name.isEmpty()) {
+			// try to read in the file
+			Path path = Paths.get(name);
+			Path abs = path.toAbsolutePath();
+			try {
+				return Files.readAllBytes(path);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				System.err.println("Failed to read class file");
+			}
+		} 
+		// default returning null
+		return null;
+	}
 }
