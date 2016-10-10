0e7a97323e614a2957b8d8c6bd72f7ca2125e5a5
Kyle <traverskyle9@gmail.com>
2016-10-09 21:38:59 -0400

    Adding part 1.1

---
+++ homework03/src/classVerifier/Pass11.java
@@ -0,0 +1,12 @@
+package classVerifier;
+
+/**
+ * Simple class that will have binary class file modified.
+ * 
+ * @author Kyle
+ */
+public class Pass11 {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+}
