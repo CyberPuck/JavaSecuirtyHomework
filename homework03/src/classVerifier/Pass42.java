d21b674314c4b03a7eff3a109a3a3f3b3a09cde5
Kyle <traverskyle9@gmail.com>
2016-10-12 23:48:23 -0400

    Adding pass 4 class

--- homework03/src/classVerifier/Pass42.java
+++ homework03/src/classVerifier/Pass42.java
@@ -1,10 +1,6 @@
 package classVerifier;
 
-/**
- * Simple class that will have binary class file modified.
- * 
- * @author Kyle
- */
+
 public class Pass42 {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
