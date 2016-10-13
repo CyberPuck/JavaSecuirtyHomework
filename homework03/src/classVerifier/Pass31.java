2f4a9e13b3abe65f96c5640c3cf6d78fe5103d83
Kyle <traverskyle9@gmail.com>
2016-10-12 23:47:59 -0400

    Adding pass 3 classes

--- homework03/src/classVerifier/Pass31.java
+++ homework03/src/classVerifier/Pass31.java
@@ -1,5 +1,16 @@
 package classVerifier;
 
+/**
+ * Break bytecode verification.
+ * 
+ * @author Kyle
+ */
 public class Pass31 {
-
+	public static void main(String[] args) {
+		int x;
+		int y = 5;
+		y++;
+		x = 5;
+		System.out.println(y + ", " + x);
+	}
 }
--- homework03/src/classVerifier/Pass32.java
+++ homework03/src/classVerifier/Pass32.java
@@ -1,5 +1,16 @@
 package classVerifier;
 
+/**
+ * Break bytecode verification, bytecode will be malformed.
+ * 
+ * @author Kyle
+ */
 public class Pass32 {
-
+	public static void main(String[] args) {
+		int x;
+		int y = 5;
+		y++;
+		x = 5;
+		System.out.println(y + ", " + x);
+	}
 }
