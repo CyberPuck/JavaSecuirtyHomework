e2fa200b2a5b17c4e3328f8a2a5af9d862ef277e
Kyle <traverskyle9@gmail.com>
2016-11-13 15:49:54 -0500

    Adding UI controllers for client

---
+++ homework05/src/client/ClientUILayoutController.java
@@ -0,0 +1,27 @@
+package client;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+
+import javafx.scene.control.TextArea;
+
+import javafx.scene.control.ChoiceBox;
+
+public class ClientUILayoutController {
+	@FXML
+	private Button loginBtn;
+	@FXML
+	private Button exportBtn;
+	@FXML
+	private TextArea rxField;
+	@FXML
+	private Button clearDisplayBtn;
+	@FXML
+	private TextArea msgField;
+	@FXML
+	private ChoiceBox clearanceComboBox;
+	@FXML
+	private Button sendMsgBtn;
+
+}
---
+++ homework05/src/client/ServerLoginPopupController.java
@@ -0,0 +1,23 @@
+package client;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+
+import javafx.scene.control.TextField;
+
+public class ServerLoginPopupController {
+	@FXML
+	private TextField serverAddressField;
+	@FXML
+	private TextField portNumberField;
+	@FXML
+	private TextField userNameField;
+	@FXML
+	private TextField userPasswordField;
+	@FXML
+	private Button loginBtn;
+	@FXML
+	private Button cancelBtn;
+
+}
