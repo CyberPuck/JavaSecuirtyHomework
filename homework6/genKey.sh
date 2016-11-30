57d207f5357573388cff295b0f353ddd7af44701
Kyle <traverskyle9@gmail.com>
2016-11-30 00:31:59 -0500

    Adding in script to generate DSA key

--- homework6/genKey.sh
+++ homework6/genKey.sh
@@ -1,1 +1,3 @@
 # Generate DSA key pair
+# genKey.sh <key store> <key store password> <alias of key>
+keytool -keystore $1 -storepass $2 -genkeypair -alias $3 -keyalg DSA -keysize 1024 -keypass $2 -dname "o=jhu.edu, cn=Kyle Travers"
